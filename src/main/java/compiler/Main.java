ece2633035b31a49d782deec73c6722fb8ab72f5
manospits <mnls95@gmail.com>
2017-03-15 16:14:49 +0200

    making initial main

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -1,10 +1,30 @@
 
 package compiler;
+//import compiler.parser.*;
+//import compiler.lexer.*;
+//import compiler.node.*;
+//import java.io.*;
 
-class Main { 
-
-	void main(String args[]) { 
-
-	}
-
+class Main{
+    public static void main(String[] arguments)
+    {
+        //try
+        //{
+            //System.out.println("Type an arithmetic expression:");
+            //// Create a Parser instance.
+            //Parser p =
+                //new Parser(
+                        //new Lexer(
+                            //new PushbackReader(
+                                //new InputStreamReader(System.in), 1024)));
+            //// Parse the input.
+            //Start tree = p.parse();
+            //// Apply the translation.
+            //tree.apply(new Translation());
+        //}
+        //catch(Exception e)
+        //{
+            //System.out.println(e.getMessage());
+        //}
+    }
 }
