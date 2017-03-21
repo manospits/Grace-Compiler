3820eab992c86769c41a6e45e94c755ce6d3962d
manospits <mnls95@gmail.com>
2017-03-21 16:35:05 +0200

    added new productions created parser for grammar in main

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -1,28 +1,49 @@
 
 package compiler;
+import compiler.parser.*;
 import compiler.lexer.*;
 import compiler.node.*;
 import java.io.*;
 
 class Main{
-    public static void main(String[] arguments)
-    {
-        PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
-        Lexer lexer = new Lexer(reader);
+    //public static void main(String[] arguments)
+    //{
+        //PushbackReader reader = new PushbackReader(new InputStreamReader(System.in));
+        //Lexer lexer = new Lexer(reader);
 
-        for(;;) {
-            try {
-                Token t = lexer.next();
+        //for(;;) {
+            //try {
+                //Token t = lexer.next();
 
-                if (t instanceof EOF)
-                    break;
-                System.out.println(t.toString());
-            } catch (Exception e)
-            {
-                System.err.println(e.getMessage());
+                //if (t instanceof EOF)
+                    //break;
+                //System.out.println(t.toString());
+            //} catch (Exception e)
+            //{
+                //System.err.println(e.getMessage());
+            //}
+        //}
+
+        //System.exit(0);
+    //}
+    public static void main(String[] arguments){
+        try{
+            Parser p =
+                new Parser(
+                new Lexer(
+                new PushbackReader(
+                new InputStreamReader(System.in), 1024)));
+            // Parse the input.
+            try{
+                Start tree = p.parse();
+                System.out.println(tree.toString());
+              }
+            catch(Exception e){
+                e.printStackTrace();
             }
         }
-
-        System.exit(0);
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }
