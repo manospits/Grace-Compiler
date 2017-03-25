675453fa0d12c3af3361b2ac00672fa6f038564b
manospits <mnls95@gmail.com>
2017-03-25 20:02:39 +0200

    added more prints in ast

--- src/main/java/compiler/PrintingVisitor.java
+++ src/main/java/compiler/PrintingVisitor.java
@@ -66,7 +66,7 @@ public class PrintingVisitor extends DepthFirstAdapter{
         System.out.println("Fpar-def start");
         spaces+=1;
         print_spaces();
-        System.out.printf("Ref : ");
+        System.out.printf("<ref> : ");
         if(node.getTRef() != null)
         {
             System.out.printf("Yes\n");
@@ -76,7 +76,7 @@ public class PrintingVisitor extends DepthFirstAdapter{
 
         }
         print_spaces();
-        System.out.printf("IDs : %s", node.getTId().toString());
+        System.out.printf("<ids> : %s", node.getTId().toString());
         List<PIdComma> copy = new ArrayList<PIdComma>(node.getIdComma());
         for(PIdComma e : copy)
         {
@@ -84,7 +84,7 @@ public class PrintingVisitor extends DepthFirstAdapter{
         }
         System.out.println("");
         print_spaces();
-        System.out.printf("Fpar-type : %s \n",node.getFparType().toString());
+        System.out.printf("<fpar-type> : %s \n",node.getFparType().toString());
     }
 
     @Override
@@ -111,4 +111,123 @@ public class PrintingVisitor extends DepthFirstAdapter{
         System.out.println("Local-def end");
     }
 
+    //func-decl
+    @Override
+    public void inAFuncDecl(AFuncDecl node){
+        print_spaces();
+        System.out.println("Func-decl start");
+        spaces+=1;
+    }
+
+    @Override
+    public void outAFuncDecl(AFuncDecl node){
+        spaces-=1;
+        print_spaces();
+        System.out.println("Func-decl end");
+    }
+
+    //var-def
+    @Override
+    public void inAVarDef(AVarDef node)
+    {
+        print_spaces();
+        System.out.println("Var-def start");
+        spaces+=1;
+        print_spaces();
+        System.out.printf("<ids>  : %s", node.getTId().toString());
+        List<PIdComma> copy = new ArrayList<PIdComma>(node.getIdComma());
+        for(PIdComma e : copy)
+        {
+            System.out.printf("%s",e.toString());
+        }
+        System.out.println("");
+        print_spaces();
+        System.out.printf("<type> : %s \n",node.getType().toString());
+    }
+
+    @Override
+    public void outAVarDef(AVarDef node)
+    {
+        spaces-=1;
+        print_spaces();
+        System.out.println("Var-def end");
+    }
+
+    //block
+    @Override
+    public void inABlock(ABlock node){
+        print_spaces();
+        System.out.println("Block start");
+        spaces+=1;
+    }
+
+    @Override
+    public void outABlock(ABlock node){
+        spaces-=1;
+        print_spaces();
+        System.out.println("Block end");
+    }
+
+    //func call
+    @Override
+    public void inAFuncCall(AFuncCall node){
+        print_spaces();
+        System.out.println("Func-call start");
+        spaces+=1;
+        print_spaces();
+        System.out.printf("Function <id> : %s\n",node.getTId().toString());
+    }
+
+    @Override
+    public void outAFuncCall(AFuncCall node){
+        spaces-=1;
+        print_spaces();
+        System.out.println("Func-call end");
+    }
+
+    @Override
+    public void inAStmtLvalueStmt(AStmtLvalueStmt node){
+        print_spaces();
+        spaces+=1;
+        System.out.println("Assignment");
+        print_spaces();
+        System.out.printf("<l-value> : %s\n",node.getLValue().toString());
+    }
+
+    @Override
+    public void outAStmtLvalueStmt(AStmtLvalueStmt node){
+        spaces-=1;
+        print_spaces();
+        System.out.printf("Assignment end\n");
+    }
+
+    @Override
+    public void inAStmtElseLvalueStmtElse(AStmtElseLvalueStmtElse node){
+        print_spaces();
+        spaces+=1;
+        System.out.println("Stmt : Assignment");
+        print_spaces();
+        System.out.printf("<l-value> : %s\n",node.getLValue().toString());
+    }
+
+    @Override
+    public void outAStmtElseLvalueStmtElse(AStmtElseLvalueStmtElse node){
+        spaces-=1;
+        print_spaces();
+        System.out.printf("Assignment end\n");
+    }
+
+    @Override
+    public void inAIf(AIf node){
+        print_spaces();
+        System.out.println("If starting");
+        spaces+=1;
+    }
+
+    @Override
+    public void outAIf(AIf node){
+        spaces-=1;
+        print_spaces();
+        System.out.printf("If end\n");
+    }
 }
