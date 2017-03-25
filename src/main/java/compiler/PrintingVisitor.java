f468bffd66b62dc5a7396fa7ed00534cf14a9bdd
manospits <mnls95@gmail.com>
2017-03-25 15:27:15 +0200

    added spaces and more prints

--- src/main/java/compiler/PrintingVisitor.java
+++ src/main/java/compiler/PrintingVisitor.java
@@ -5,46 +5,89 @@ import compiler.node.*;
 import java.io.*;
 
 public class PrintingVisitor extends DepthFirstAdapter{
+    int spaces=0;
+
+    public void print_spaces(){
+        for(int i=0;i<spaces;i++){
+            System.out.printf("  ");
+        }
+    }
+
+    //program
     @Override
     public void inAProgram(AProgram node){
+        print_spaces();
         System.out.println("Program start");
+        spaces+=1;
     }
 
     @Override
     public void outAProgram(AProgram node){
+        spaces-=1;
+        print_spaces();
         System.out.println("Program end");
     }
 
+    //func-def
     @Override
     public void inAFuncDef(AFuncDef node){
+        print_spaces();
         System.out.println("Func-def start");
+        spaces+=1;
     }
 
     @Override
     public void outAFuncDef(AFuncDef node){
+        spaces-=1;
+        print_spaces();
         System.out.println("Func-def end");
     }
 
+    //header
     @Override
     public void inAHeader(AHeader node){
+        print_spaces();
         System.out.printf("Header start : <id> = %s <ret-type> = %s \n", node.getTId().toString(),node.getRetType().toString());
+        spaces+=1;
     }
 
     @Override
     public void outAHeader(AHeader node){
+        spaces-=1;
+        print_spaces();
         System.out.printf("Header end : <id> = %s\n", node.getTId().toString());
     }
 
+    //fpar-def
     @Override
-    public void inAWithrefFparDef(AWithrefFparDef node)
-    {
+    public void inAFparDef(AFparDef node){
+        print_spaces();
         System.out.println("Fpar-def start");
+        spaces+=1;
     }
 
     @Override
-    public void outAWithrefFparDef(AWithrefFparDef node)
-    {
+    public void outAFparDef(AFparDef node){
+        spaces-=1;
+        print_spaces();
         System.out.println("Fpar-def end");
+    }
+
+    //local-def
+    @Override
+    public void inAFuncDefLocalDef(AFuncDefLocalDef node)
+    {
+        print_spaces();
+        System.out.println("Local-def start");
+        spaces+=1;
+    }
+
+
+    @Override
+    public void outAFuncDefLocalDef(AFuncDefLocalDef node){
+        spaces-=1;
+        print_spaces();
+        System.out.println("Local-def end");
     }
 
 }
